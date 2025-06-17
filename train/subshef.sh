0a95de8b3fb074ec3242f77c8744eb940cf939d5
weihezhai <weihezhai@gmail.com>
2025-06-17 19:15:56 +0100

    debug

--- train/subshef.sh
+++ train/subshef.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
-#SBATCH --gres=gpu:h100:1
+#SBATCH --gres=gpu:2
 #SBATCH --mem=96G
 #SBATCH --cpus-per-task=12
 #SBATCH --mail-user=wzhai2@sheffield.ac.uk

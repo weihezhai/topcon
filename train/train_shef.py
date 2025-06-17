1ccad341c69a52b2b03671c41c6afbf30b32a04b
weihezhai <weihezhai@gmail.com>
2025-06-17 18:10:37 +0100

    debug

--- train/train_shef.py
+++ train/train_shef.py
@@ -213,7 +213,7 @@ def main():
         output_dir=OUTPUT_DIR,
         num_train_epochs=5,
         per_device_train_batch_size=1,  # Adjust based on GPU memory
-        per_device_eval_batch_size=4,
+        per_device_eval_batch_size=1,
         gradient_accumulation_steps=8,
         warmup_steps=100,
         weight_decay=0.01,

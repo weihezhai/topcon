98839d785e901289b0605dd88eff1d7d1e443346
weihezhai <weihezhai@gmail.com>
2025-06-17 19:15:20 +0100

    debug

--- train/subshef.sh
+++ train/subshef.sh
@@ -16,7 +16,7 @@ module load cuDNN/8.9.2.26-CUDA-12.1.1
 # module load cuDNN/8.7.0.84-CUDA-11.8.0
 # Activate your conda environment
 # source activate etu
-source activate qwen-env
+source activate qwen
 
 # Run the preprocessing script using the runner
 # You can modify these parameters as needed

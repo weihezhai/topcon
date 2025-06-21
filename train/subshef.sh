50da312243624fe63e7c2f8b426264862e41c184
weihezhai <weihezhai@gmail.com>
2025-06-22 00:06:24 +0100

    sub

--- train/subshef.sh
+++ train/subshef.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --qos=gpu
-#SBATCH --gres=gpu:3
-#SBATCH --mem=96G
+#SBATCH --gres=gpu:2
+#SBATCH --mem=90G
 #SBATCH --cpus-per-task=12
 #SBATCH --mail-user=wzhai2@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --job-name=iclr_2025_llm_paper_qwen3_8B_lora
-#SBATCH --output=./logs/iclr_2025_llm_paper_qwen3_8B_lora.%j.out
+#SBATCH --job-name=iclr_2025_llm_paper_qwen3_1d7b
+#SBATCH --output=./logs/iclr_2025_llm_paper_qwen3_1d7b.%j.out
 #SBATCH --time=0-12:00:00
 
 # Load necessary modules
-module load Anaconda3/2024.02-1
+# module load Anaconda3/2024.02-1
 # module load GCC/11.2.0
 # module load cuDNN/8.7.0.84-CUDA-11.8.0
+module load Python/3.10.8-GCCcore-12.2.0
 module load cuDNN/8.9.2.26-CUDA-12.1.1
 # module load CUDA/12.4.0
 
 # Activate your conda environment
 # source activate etu
-source activate training_env
+source envs/py310/bin/activate
 
 # Run the preprocessing script using the runner
 # You can modify these parameters as needed
-deepspeed --num_gpus=3 train_shef.py
+python train_shef_abs_intro.py

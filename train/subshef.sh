741cca5398193a888c825c0fc1c663e952392005
weihezhai <weihezhai@gmail.com>
2025-06-27 21:56:07 +0100

    debug

--- train/subshef.sh
+++ train/subshef.sh
@@ -24,4 +24,4 @@ source /mnt/parscratch/users/acr24wz/envs/py310/bin/activate
 
 # Run the preprocessing script using the runner
 # You can modify these parameters as needed
-python train_shef_abs_intro.py
+python train_shef_abs_intro.py --detailed_eval

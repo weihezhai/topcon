d47685e6da387e0aed23a66de2ff0e0be7a964cb
weihezhai <weihezhai@gmail.com>
2025-06-29 19:52:22 +0100

    switch to accelerator

---
+++ train/subs/deepseek/launch_eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the specific GPU IDs you want to use
+export CUDA_VISIBLE_DEVICES="0,1,2"
+
+# Launch evaluation with accelerate
+accelerate launch \
+    --config_file /data/scratch/mpx602/topcon-1/train/subs/deepseek/accelerate_config.yaml \
+    /data/scratch/mpx602/topcon-1/train/subs/deepseek/train_shef_abs_intro.py \
+    --eval \
+    --detailed_eval \
+    --model_name "deepseek-ai/DeepSeek-R1-0528-Qwen3-8B" \
+    --data_folder "/mnt/parscratch/users/acr24wz/src/iclr/data/scratch/mpx602/topcon-1/conference_data/iclr_2025_data/filtered_llm_papers/llm_papers_text/" \
+    --labels_file "/mnt/parscratch/users/acr24wz/topcon/train/llm_paper/label_simple.json" \
+    --output_dir "/mnt/parscratch/users/acr24wz/etu/topcon/DeepSeek-R1-0528-Qwen3-8B/finetuned_model" \
+    --max_length 10000

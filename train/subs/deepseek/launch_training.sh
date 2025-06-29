4139330fdbd4a20751dce0481168b4921797a976
weihezhai <weihezhai@gmail.com>
2025-06-29 20:01:10 +0100

    debug

--- train/subs/deepseek/launch_training.sh
+++ train/subs/deepseek/launch_training.sh
@@ -5,8 +5,8 @@ export CUDA_VISIBLE_DEVICES="0,1,3"
 
 # Launch training with accelerate
 accelerate launch \
-    --config_file /data/scratch/mpx602/topcon-1/train/subs/deepseek/accelerate_config.yaml \
-    /data/scratch/mpx602/topcon-1/train/subs/deepseek/train_shef_abs_intro.py \
+    --config_file /mnt/parscratch/users/acr24wz/topcon/train/subs/deepseek/accelerate_config.yaml \
+    /mnt/parscratch/users/acr24wz/topcon/train/subs/deepseek/train_shef_abs_intro.py \
     --model_name "deepseek-ai/DeepSeek-R1-0528-Qwen3-8B" \
     --data_folder "/mnt/parscratch/users/acr24wz/src/iclr/data/scratch/mpx602/topcon-1/conference_data/iclr_2025_data/filtered_llm_papers/llm_papers_text/" \
     --labels_file "/mnt/parscratch/users/acr24wz/topcon/train/llm_paper/label_simple.json" \

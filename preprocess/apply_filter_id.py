4fe8a018ae75336533403855b3724aec1c09e415
weihezhai <weihezhai@gmail.com>
2025-07-18 11:34:59 +0100

    all domain

--- preprocess/apply_filter_id.py
+++ preprocess/apply_filter_id.py
@@ -119,9 +119,9 @@ if __name__ == "__main__":
     import sys
     
     # Default configuration
-    paper_ids_file = "/data/scratch/mpx602/topcon-1/conference_data/iclr_2025_data/filtered_theory_papers/matched_paper_ids.json"
+    paper_ids_file = "/data/scratch/mpx602/topcon-1/conference_data/iclr_2025_data/all_four_domain/matched_paper_ids.json"
     txt_source_dir = "/data/scratch/mpx602/topcon-1/conference_data/iclr_2025_data/txt_outputs"
-    output_dir = "/data/scratch/mpx602/topcon-1/conference_data/iclr_2025_data/filtered_theory_papers/theory_papers_text"
+    output_dir = "/data/scratch/mpx602/topcon-1/conference_data/iclr_2025_data/all_four_domain/four_domain_text"
 
     # Allow command line arguments
     if len(sys.argv) >= 2:

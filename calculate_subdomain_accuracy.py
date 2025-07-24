eb626a4c9a43959feb0215fad71ffdef76893723
weihezhai <weihezhai@gmail.com>
2025-07-24 15:32:07 +0100

    name

--- calculate_subdomain_accuracy.py
+++ calculate_subdomain_accuracy.py
@@ -151,4 +151,4 @@ if __name__ == "__main__":
     print_results(results)
     
     # Save results to file
-    save_results(results, "subdomain_accuracy_results.json")
+    save_results(results, "cv_1.7_subdomain_accuracy_results.json")

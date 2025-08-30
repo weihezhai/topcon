bc13d2408cd7544e952e9a40d95e060c1a500229
weihezhai <weihezhai@gmail.com>
2025-08-30 23:46:23 +0200

    8b

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # topcon
 topcon.ai
+
+# storage 5T
+/ceph/hpc/data/d2025d08-005-users
+
--- train/8b/subllm.sh
+++ train/8b/subllm.sh
@@ -6,9 +6,9 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mail-user=wzhai2@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --job-name=iclr_2025_cv_paper_qwen3_4b
-#SBATCH --output=./logs/iclr_2025_cv_paper_qwen3_4b.%j.out
-#SBATCH --time=0-12:00:00
+#SBATCH --job-name=llm_4b
+#SBATCH --output=./logs/llm_4b.%j.out
+#SBATCH --time=0-6:00:00
 
 # Load necessary modules
 # module load Anaconda3/2024.02-1
@@ -16,12 +16,12 @@
 # module load cuDNN/8.7.0.84-CUDA-11.8.0
 module load Python/3.10.8-GCCcore-12.2.0
 module load cuDNN/8.9.2.26-CUDA-12.1.1
+source /mnt/parscratch/users/acr24wz/envs/py310/bin/activate
 # module load CUDA/12.4.0
 
 # Activate your conda environment
 # source activate etu
-source /mnt/parscratch/users/acr24wz/envs/py310/bin/activate
 
 # Run the preprocessing script using the runner
 # You can modify these parameters as needed
-python train_shef_abs_intro_2gpu.py --detailed_eval
+python train_shef_abs_intro_two_a100_llm.py --detailed_eval

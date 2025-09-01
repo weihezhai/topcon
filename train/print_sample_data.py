c643a851e58597bc7ee336ae6a5dc7daf5fdab4a
weihezhai <weihezhai@gmail.com>
2025-09-01 13:52:58 +0200

    check data

--- train/print_sample_data.py
+++ train/print_sample_data.py
@@ -60,7 +60,7 @@ def print_random_samples(dataset_path, num_samples=20, output_file=None):
 
 if __name__ == "__main__":
     # Update this path to your cached dataset location
-    dataset_path = "/mnt/parscratch/users/acr24wz/etu/topcon/processed_dataset/llm_with_stats_with_titles"
+    dataset_path = "/mnt/parscratch/users/acr24wz/etu/topcon/processed_dataset/llm_json_with_stats"
     
     # Option 1: Print to terminal (might be cut off by terminal buffer)
     print_random_samples(dataset_path, num_samples=20)

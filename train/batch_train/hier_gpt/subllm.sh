9602c7d68edd90d302923c04882df5f7ae204de2
weihezhai <weihezhai@gmail.com>
2025-09-03 01:57:01 +0200

    rename

--- train/batch_train/hier_gpt/subllm.sh
+++ train/batch_train/hier_gpt/subllm.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mail-user=wzhai2@sheffield.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --job-name=llm_4b
+#SBATCH --job-name=llm_4b_hier
 #SBATCH --output=./logs/llm_4b_hier.%j.out
 #SBATCH --time=0-12:00:00
 

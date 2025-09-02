635716ac0192e72ded287806f613c4fca58de03d
weihezhai <weihezhai@gmail.com>
2025-09-02 14:24:50 +0200

    1epoch

--- train/batch_train/hier_gpt/main.py
+++ train/batch_train/hier_gpt/main.py
@@ -495,7 +495,7 @@ def main():
             # base (Stage-1) training args
             training_args = TrainingArguments(
                 output_dir=OUTPUT_DIR,
-                num_train_epochs=3,
+                num_train_epochs=1,
                 per_device_train_batch_size=1,
                 per_device_eval_batch_size=1,
                 gradient_accumulation_steps=8,

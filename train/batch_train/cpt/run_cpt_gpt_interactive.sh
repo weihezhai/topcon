27d5a9a1dc747aea22b76e1660ea49ed09255fe4
weihezhai <weihezhai@gmail.com>
2025-09-03 18:12:28 +0200

    interact

--- train/batch_train/cpt/run_cpt_gpt_interactive.sh
+++ train/batch_train/cpt/run_cpt_gpt_interactive.sh
@@ -34,7 +34,7 @@ python cpt_gpt.py \
     --bf16 \
     --num_proc 4 \
     --eval_holdout 0.1 \
-    --gpu_ids 0 1 \
+    --gpu_ids 1 3 \
     --flash_attn \
     --use_dataset_builder \
     --use_cache \

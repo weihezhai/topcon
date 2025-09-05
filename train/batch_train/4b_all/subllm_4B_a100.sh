2b3ec2e843e83865a11796552f9d5c9bba59f08b
weihezhai <weihezhai@gmail.com>
2025-09-05 16:56:19 +0200

    a100

---
+++ train/batch_train/4b_all/subllm_4B_a100.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=12
+#SBATCH --mail-user=wzhai2@sheffield.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name=llm_4b_main_body
+#SBATCH --output=./logs/llm_4b_main_body.%j.out
+#SBATCH --time=0-18:00:00
+
+# Load necessary modules
+# module load Anaconda3/2024.02-1
+# module load GCC/11.2.0
+# module load cuDNN/8.7.0.84-CUDA-11.8.0
+module load Python/3.10.8-GCCcore-12.2.0
+module load cuDNN/8.9.2.26-CUDA-12.1.1
+source /mnt/parscratch/users/acr24wz/envs/py310/bin/activate
+# module load CUDA/12.4.0
+
+# Activate your conda environment
+# source activate etu
+
+# Run the preprocessing script using the runner
+# You can modify these parameters as needed
+python 4B_all_two_a100_llm.py --detailed_eval

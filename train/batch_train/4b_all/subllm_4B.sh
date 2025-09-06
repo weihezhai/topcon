fc1086f2ac8740aebb151717bd7203f15ce7ba40
weihezhai <weihezhai@gmail.com>
2025-09-06 17:05:22 +0200

    debug

--- train/batch_train/4b_all/subllm_4B.sh
+++ train/batch_train/4b_all/subllm_4B.sh
@@ -24,4 +24,4 @@ source /mnt/parscratch/users/acr24wz/envs/py310/bin/activate
 
 # Run the preprocessing script using the runner
 # You can modify these parameters as needed
-python 4B_all_two_a100_llm.py --detailed_eval
+python 4BINST_all_two_a100_llm.py --detailed_eval

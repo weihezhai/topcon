08eb7faaf25d5c1b04ce6e6ab5ce93c16f7c84b9
weihezhai <weihezhai@gmail.com>
2025-09-11 10:23:50 +0200

    seqcls batch

---
+++ train/batch_train/8b_all/cpted/seqcls/subllm_8B.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --mem=120G
+#SBATCH --cpus-per-task=12
+#SBATCH --mail-user=wzhai2@sheffield.ac.uk
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name=seqcls_cpted_llm_8b_8000
+#SBATCH --output=./logs/seqcls_cpted_llm_8b_8000.%j.out
+#SBATCH --time=0-10:00:00
+
+# Load necessary modules
+# module load Anaconda3/2024.02-1
+# module load GCC/11.2.0
+# module load cuDNN/8.7.0.84-CUDA-11.8.0
+module load Python/3.10.8-GCCcore-12.2.0
+module load cuDNN/8.9.2.26-CUDA-12.1.1
+source /mnt/parscratch/users/acr24wz/envs/py310/bin/activate
+# module load CUDA/12.4.0
+
+# Activate your conda environment
+# source activate etu
+
+# Run the preprocessing script using the runner
+# You can modify these parameters as needed
+python seqcls_8B_8000_two_a100_llm.py --detailed_eval

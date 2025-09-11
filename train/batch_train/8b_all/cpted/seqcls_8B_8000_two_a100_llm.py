3be1a29fce520091014384920cf0d097f31d77d4
weihezhai <weihezhai@gmail.com>
2025-09-11 11:55:31 +0200

    debug

--- train/batch_train/8b_all/cpted/seqcls_8B_8000_two_a100_llm.py
+++ train/batch_train/8b_all/cpted/seqcls_8B_8000_two_a100_llm.py
@@ -332,7 +332,7 @@ def main():
                 num_train_epochs=3,
                 per_device_train_batch_size=2,
                 per_device_eval_batch_size=1,
-                gradient_accumulation_steps=4,
+                gradient_accumulation_steps=8,
                 learning_rate=2e-5,
                 warmup_steps=200,
                 weight_decay=0.01,

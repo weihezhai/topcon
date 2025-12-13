3d9403f75a8c8125aa99526440d8fb48d2e0699d
weihezhai <weihezhai@gmail.com>
2025-12-14 00:55:30 +0100

    debug

--- tri_label/tri_4B_12000_two_a100_llm_vl.py
+++ tri_label/tri_4B_12000_two_a100_llm_vl.py
@@ -883,7 +883,7 @@ def main():
                 model=model,
                 args=training_args,
                 train_dataset=train_dataset,
-                eval_dataset=small_eval_dataset  # Use smaller eval dataset for periodic evaluation
+                eval_dataset=small_eval_dataset,
                 tokenizer=tokenizer,
                 data_collator=data_collator,
                 preprocess_logits_for_metrics=preprocess_logits_for_metrics,

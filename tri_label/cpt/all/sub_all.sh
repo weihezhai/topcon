473a7af3527dd36aaa8b21806460e7099419c0e1
weihezhai <weihezhai@gmail.com>
2025-12-15 00:39:05 +0100

    all

---
+++ tri_label/cpt/all/sub_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=gpu-h100-nvl
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=100G
+#SBATCH --cpus-per-task=12
+#SBATCH --mail-user=weihezhai@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name=wl_4b_vl_cpt_all
+#SBATCH --output=./logs/wl_4b_vl_cpt_all.%j.out
+#SBATCH --time=0-24:00:00
+
+module load cuDNN/8.7.0.84-CUDA-11.8.0
+source /mnt/parscratch/users/lip22fh/miniconda3/etc/profile.d/conda.sh
+conda activate pap
+
+python 4B_12000_all_vl_wl.py --detailed_eval

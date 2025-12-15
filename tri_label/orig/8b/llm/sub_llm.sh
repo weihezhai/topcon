e372d21ff625e32b06a31f18f6ce0851711f8e26
weihezhai <weihezhai@gmail.com>
2025-12-15 17:39:54 +0100

    8b

---
+++ tri_label/orig/8b/llm/sub_llm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=gpu-h100-nvl
+#SBATCH --qos=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --mem=200G
+#SBATCH --cpus-per-task=12
+#SBATCH --mail-user=weihezhai@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name=wl_8b_vl_orig_llm
+#SBATCH --output=./logs/wl_8b_vl_orig_llm.%j.out
+#SBATCH --time=0-10:00:00
+
+module load cuDNN/8.7.0.84-CUDA-11.8.0
+source /mnt/parscratch/users/lip22fh/miniconda3/etc/profile.d/conda.sh
+conda activate pap
+
+python 8B_12000_llm_vl_wl.py --detailed_eval

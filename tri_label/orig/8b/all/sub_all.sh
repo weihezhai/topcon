4ea8a998c550a4e0c77e441f133bad1a10fa65b1
weihezhai <weihezhai@gmail.com>
2025-12-16 01:27:00 +0100

    all

--- tri_label/orig/8b/all/sub_all.sh
+++ tri_label/orig/8b/all/sub_all.sh
@@ -6,12 +6,12 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --mail-user=weihezhai@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --job-name=wl_4b_vl_cpt_all
-#SBATCH --output=./logs/wl_4b_vl_cpt_all.%j.out
+#SBATCH --job-name=wl_8b_vl_orig_all
+#SBATCH --output=./logs/wl_8b_vl_orig_all.%j.out
 #SBATCH --time=0-24:00:00
 
 module load cuDNN/8.7.0.84-CUDA-11.8.0
 source /mnt/parscratch/users/lip22fh/miniconda3/etc/profile.d/conda.sh
 conda activate pap
 
-python 4B_12000_all_vl_wl.py --detailed_eval
+python 8B_12000_all_vl_wl.py --detailed_eval
